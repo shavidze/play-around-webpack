0aa4c69a66006620bbebaf749caddbfdbac53d97
Saba Shavidze <sabashavidze@gmail.com>
2023-06-27 11:50:01 +0400

    add html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = ({ mode }) => ({
   mode,
+  output: {
+    filename: "bundle.js",
+  },
+  plugins: [new HtmlWebpackPlugin()],
 });

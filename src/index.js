ab036f8238a6e959e6374eb3ca061cb476793714
Saba Shavidze <sabashavidze@gmail.com>
2023-06-27 12:12:16 +0400

    create a first button and show it to the screent with webpack

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import nav from "./nav";
 import { top, bottom } from "./foo";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
-document.body.append(makeButton("My First Button"));
-makeButton("My First Button");
+const btn = makeButton("My First Button");
+btn.style = makeColorStyle("blue");
+document.body.appendChild(btn);
